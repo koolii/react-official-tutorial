0fd2159a37a66853f815260c214999677b951de9
koolii <koolii0909@gmail.com>
2017-12-03 00:33:28 +0100

    add based Game class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Game from './game.js';
 import './index.css';
 
 class ShoppingList extends React.Component {
@@ -7,11 +8,12 @@ class ShoppingList extends React.Component {
     return (
       <div className="shopping-list">
         <h1>shopping list for {this.props.name}</h1>
-	<ul>
-	  <li>Instagram</li>
-	  <li>WhatsApp</li>
-	  <li>Oculus</li>
-	</ul>
+      	<ul>
+      	  <li>Instagram</li>
+      	  <li>WhatsApp</li>
+      	  <li>Oculus</li>
+      	</ul>
+        <Game />
      </div>
     );
   }
