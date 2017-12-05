33a38113fe0937a204b6f6a83e00541ee58b6506
koolii <kuriyama@createentrance.com>
2017-12-05 08:18:36 +0100

    implement history

--- src/game.js
+++ src/game.js
@@ -80,11 +80,21 @@ export default class Game extends React.Component {
     });
   }
 
-
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
-    const winner = calculateWinner(current.state);
+    const winner = calculateWinner(current.squares);
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move:
+        'Go to game start';
+
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    })
 
     let status;
     if (winner) {
@@ -103,19 +113,12 @@ export default class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
   }
 }
-
-// ========================================
-
-// ReactDOM.render(
-//   <Game />,
-//   document.getElementById('root')
-// );
 
 function calculateWinner(squares) {
   const lines = [
